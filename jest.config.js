7e1d40f373f971268a555c663d4772a4e709850d
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-11-02 19:17:21 +0200

    update

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    testMatch: [
+      "**/?(*.)+(spec|test).[tj]s?(x)",  
+      "**/__tests__/**/*.[jt]s?(x)"      
+  ],
   };
   
