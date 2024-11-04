bc0334d7d41deafd80de34ab51c65ba45dc3629b
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-11-04 23:30:48 +0200

    filter cloud

--- src/api/controllers/filterController.ts
+++ src/api/controllers/filterController.ts
@@ -1,57 +1,48 @@
-import { Request, Response, NextFunction } from 'express'
-import sharp from 'sharp'
-import fs from 'fs'
-import path from 'path'
-import { ErrorMessages } from '../../utils/constants'
-
-const uploadDir = path.join(__dirname, '../../uploads')
-
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir)
-}
+import { Request, Response, NextFunction } from 'express';
+import sharp from 'sharp';
+import { ErrorMessages } from '../../utils/constants';
+import { v2 as cloudinary } from 'cloudinary';
 
 export const applyGrayscale = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     if (!req.file) {
-      return next(new Error(ErrorMessages.NO_FILE_UPLOADED))
+      return next(new Error(ErrorMessages.NO_FILE_UPLOADED));
     }
 
-    const processedImage = await sharp(req.file.buffer)
+    const processedImageBuffer = await sharp(req.file.buffer)
       .grayscale()
-      .toBuffer()
+      .toBuffer();
 
-    const fileName = `grayscale_${Date.now()}.jpg`
-    const filePath = path.join(uploadDir, fileName)
+    const result = await cloudinary.uploader.upload(`data:image/jpeg;base64,${processedImageBuffer.toString('base64')}`, {
+      folder: 'uploads',
+    });
 
-    await sharp(processedImage).toFile(filePath)
-
-    res.status(200).contentType(req.file.mimetype) 
-    .send(processedImage)
+    res.status(200).json({ message: 'Grayscale image uploaded successfully!', url: result.secure_url });
   } catch (error) {
-    next(error)
+    console.error('Error in applyGrayscale:', error);
+    next(error);
   }
-}
+};
 
 export const applyBlur = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const { intensity } = req.body;
 
     if (!req.file) {
-      return next(new Error(ErrorMessages.NO_FILE_UPLOADED))
+      return next(new Error(ErrorMessages.NO_FILE_UPLOADED));
     }
 
-    const processedImage = await sharp(req.file.buffer)
-      .blur(intensity)
-      .toBuffer()
+    const processedImageBuffer = await sharp(req.file.buffer)
+      .blur(intensity || 1) 
+      .toBuffer();
 
-    const fileName = `blurred_${Date.now()}.jpg`
-    const filePath = path.join(uploadDir, fileName)
+    const result = await cloudinary.uploader.upload(`data:image/jpeg;base64,${processedImageBuffer.toString('base64')}`, {
+      folder: 'uploads',
+    });
 
-    await sharp(processedImage).toFile(filePath)
-
-    res.status(200).contentType(req.file.mimetype) 
-    .send(processedImage)
+    res.status(200).json({ message: 'Blurred image uploaded successfully!', url: result.secure_url });
   } catch (error) {
-    next(error)
+    console.error('Error in applyBlur:', error);
+    next(error);
   }
 };
