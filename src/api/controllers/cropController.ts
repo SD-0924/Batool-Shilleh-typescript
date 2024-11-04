62fc5bf0fe31363d2c71856fb29bcb8cfab98253
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-11-04 23:07:45 +0200

    crop in cloud

--- src/api/controllers/cropController.ts
+++ src/api/controllers/cropController.ts
@@ -1,40 +1,33 @@
 import { Request, Response, NextFunction } from 'express';
 import sharp from 'sharp';
-import fs from 'fs';
-import path from 'path';
-import { ErrorMessages } from '../../utils/constants'
-const uploadDir = path.join(__dirname, '../../uploads')
-
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir)
-}
+import { uploadImageToCloudinary } from '../../utils/cloudinaryService'; 
+import { ErrorMessages } from '../../utils/constants';
 
 export const cropImage = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  try {
-    const { width, height, left, top } = req.body
+    try {
+        const { width, height, left, top } = req.body;
 
-    if (!req.file) {
-      return next(new Error(ErrorMessages.NO_FILE_UPLOADED)) 
+        if (!req.file) {
+            return next(new Error(ErrorMessages.NO_FILE_UPLOADED));
+        }
+
+        if (width === undefined || height === undefined || left === undefined || top === undefined) {
+            return next(new Error(ErrorMessages.REQUIRED_FIELDS));
+        }
+
+        const croppedImageBuffer = await sharp(req.file.buffer)
+            .extract({
+                width: Number(width),
+                height: Number(height),
+                left: Number(left),
+                top: Number(top),
+            })
+            .toBuffer(); 
+
+        const imageUrl = await uploadImageToCloudinary(croppedImageBuffer);
+
+        res.status(200).json({ message: ErrorMessages.CROP_SUCCESS, url: imageUrl });
+    } catch (error) {
+        next(error);
     }
-
-    if (width === undefined || height === undefined || left === undefined || top === undefined) {
-      return next(new Error(ErrorMessages.REQUIRED_FIELDS))
-    }
-
-    const fileName = `cropped_${Date.now()}.jpg`
-    const filePath = path.join(process.cwd(), fileName)
-
-    await sharp(req.file.buffer)
-      .extract({
-        width: Number(width),
-        height: Number(height),
-        left: Number(left),
-        top: Number(top),
-      })
-      .toFile(filePath)
-
-      res.status(200).json({ message: ErrorMessages.CROP_SUCCESS, path: filePath })
-  } catch (error) {
-    next(error)
-  }
 };
