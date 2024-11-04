2f1e29a15b9a4fb4bc786d870b992ab4d459b514
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-11-05 00:43:45 +0200

    project

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,14 @@ import { errorHandler } from './api/middleware/errorMiddleware'
 import { logRequest } from './api/middleware/logMiddleware'
 
 const app: Application = express()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json())
 app.use(logRequest)
 app.use('/api/images', imageRoutes)
 app.use(errorHandler)
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
+export { app, server };
