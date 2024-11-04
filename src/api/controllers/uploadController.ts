3337df9170518e11d052c6fd4c5dd554af5a794e
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-11-04 23:05:20 +0200

    upload in cloud

--- src/api/controllers/uploadController.ts
+++ src/api/controllers/uploadController.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import sharp from 'sharp';
-import { uploadImageToCloudinary } from '../../utils/cloudinaryService'; // تأكد من المسار صحيح
+import { uploadImageToCloudinary } from '../../utils/cloudinaryService'; 
 import { ErrorMessages } from '../../utils/constants';
 
 export const uploadImage = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -9,18 +9,13 @@ export const uploadImage = async (req: Request, res: Response, next: NextFunctio
             return next(new Error(ErrorMessages.NO_FILE_UPLOADED));
         }
 
-        // استخدام Sharp لمعالجة الصورة
         const processedImageBuffer = await sharp(req.file.buffer)
             .resize(800, 800, {
-                fit: sharp.fit.cover, // تعديل الصورة لتناسب الإطار المحدد
-                position: 'center' // مركز الصورة
+                fit: sharp.fit.cover, 
+                position: 'center' 
             })
-            .toBuffer(); // الحصول على Buffer للصورة المعالجة
-
-        // رفع الصورة إلى Cloudinary
+            .toBuffer();
         const imageUrl = await uploadImageToCloudinary(processedImageBuffer);
-        
-        // إرجاع رابط الصورة
         res.status(200).json({ message: 'Image uploaded successfully!', url: imageUrl });
     } catch (error) {
         next(error);
